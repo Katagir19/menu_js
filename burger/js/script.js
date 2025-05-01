3f3d14539af2ce4d03adb9e296b61eaf98daa095
katagir19 <YaroslavN06@gmail.com>
2025-05-01 15:17:32 +0300

    end

--- burger/js/script.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('.menu__body')
-const menuBtn = document.querySelector('.menu_icon')
-const body = document.body;
-
-
---
+++ main_menu/main.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.main-menu');
+
+const body = document.body;
+
+if (burger && menu){
+    burger.addEventListener('click', () =>{
+        menu.classList.toggle('active')
+        body.classList.toggle('lock')
+        burger.classList.toggle('active')
+    });
+
+    menu.querySelectorAll('.title').forEach(event =>{
+        menu.addEventListener('click', () =>{
+            menu.classList.remove('active')
+            body.classList.remove('lock')
+            burger.classList.remove('active')
+        })
+    })
+};
+
