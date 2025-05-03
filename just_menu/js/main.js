f2c2cdb00f8fdf77e6ba2a9329c47c143605c6b4
katagir19 <YaroslavN06@gmail.com>
2025-05-03 14:25:28 +0300

    добавил улучшенное менб в (just-menu)

--- just_menu/js/main.js
+++ just_menu/js/main.js
@@ -1,10 +1,28 @@
 const menu = document.querySelector('#main-menu')
 const dropped = document.querySelector('#dropped-menu')
 
-menu.addEventListener('click', () =>{
+menu.addEventListener('click', () => {
     if (dropped.classList.contains('disp') == true) {
         dropped.classList.remove('disp');
-    } else{
+    } else {
         dropped.classList.add('disp');
-    } 
+    }
 })
+
+// ======= для поддив
+const poddivs = document.querySelectorAll('.poddiv'); 
+
+poddivs.forEach(poddiv => {
+    poddiv.addEventListener('click', (e) => {
+        // e.stopPropagation();
+        const firstItems = poddiv.querySelectorAll('.first');
+        
+        firstItems.forEach(item => {
+            if (item.classList.contains('disp')) {
+                item.classList.remove('disp');
+            } else {
+                item.classList.add('disp');
+            }
+        });
+    });
+});
